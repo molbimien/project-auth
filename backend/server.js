dc45eb993cb8baf691e9806a2e6381ce3189fb1a
molbimien <43146046+molbimien@users.noreply.github.com>
2022-01-12 15:58:33 +0100

    project startup and added routes and authenticated endpoint

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,30 @@
 import express from 'express'
 import cors from 'cors'
 import mongoose from 'mongoose'
+import crypto from 'crypto'
+import bcrypt from 'bcrypt'
 
 const mongoUrl = process.env.MONGO_URL || "mongodb://localhost/authAPI"
 mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true })
 mongoose.Promise = Promise
+
+const UserSchema = new mongoose.Schema({
+  username: {
+    type: String, 
+    unique: true, 
+    required: true,
+  },
+  password: {
+    type: String,
+    required: true, 
+  },
+  accessToken: {
+    type: String,
+    default: () => crypto.randomBytes(128).toString('hex'),
+  }
+})
+
+const User = mongoose.model('User', UserSchema)
 
 // Defines the port the app will run on. Defaults to 8080, but can be 
 // overridden when starting the server. For example:
@@ -17,9 +37,78 @@ const app = express()
 app.use(cors())
 app.use(express.json())
 
+const authenticateUser = async (req, res, next) => {
+  const accessToken = req.header('Authorization')
+
+  try {
+    const user = await User.findOne({ accessToken })
+    if (user) {
+      next()
+    } else {
+      res.status(401).json({ response: 'Please, login', success: false })
+    }
+  } catch (error) {
+    res.status(400).json({ response: error, success: false })
+  }
+}
+
 // Start defining your routes here
 app.get('/', (req, res) => {
   res.send('Hello world')
+})
+
+app.post('/signup', async (req, res) => {
+  const { username, password } = req.body
+
+  try {
+    const salt = bcrypt.genSaltSync()
+
+    if (password.length < 5) {
+      throw 'Password must be at least 5 characters long'
+    }
+
+    const newUser = await new User({
+      username,
+      password: bcrypt.hashSync(password, salt),
+    }).save()
+
+    res.status(201).json({
+      response: {
+        userId: newUser._id, 
+        username: newUser.username,
+        accessToken: newUser.accessToken
+      }, 
+      success: true
+    })
+  } catch (error) {
+    res.status(400).json({ 
+      response: error,
+      success: false
+    })
+  }
+})
+
+app.post('/signin', async (req, res) => {
+  const { username, password } = req.body
+
+  try {
+    const user = await User.findOne({ username })
+
+    if (user && bcrypt.compareSync(password, user.password)) {
+      res.status(200).json({
+        response: {
+          userId: user._id,
+          username: user.username, 
+          accessToken: user.accessToken,
+        },
+        success: true
+      })
+    } else {
+      res.status(404).json({ response: 'User not found', success: false})
+    }
+  } catch (error) {
+    res.status(400).json({ response: error, success: false })
+  }
 })
 
 // Start the server
